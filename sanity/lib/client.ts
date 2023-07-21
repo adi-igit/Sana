8cd4ba20f46e7a9d50255e13ed535e4cb04c655a
ADI IGIT <108686012+adi-igit@users.noreply.github.com>
2023-07-22 00:59:56 +0600

    Update client.ts

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    "skbJGB8ztg1CNB5uB0QwlNfVl3EepSBbYtwwZiYmJz7LbP5MFVAr02YQdDUKOFtkOl7ry14Asn7dmzvTf1xiDCBugRCiYpnh3HssFFJWGR0HmqnH1lmY4Hmmenpu45ZCVyG3PPq0tNzzICSDm44USWw0ATahYV0bMZ42uENBE19tw6SDsEB4",
 })
